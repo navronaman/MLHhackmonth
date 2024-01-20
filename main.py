04174f22705cae5248642acae84d6270f0d03525
navronaman <namanmaulikshah@gmail.com>
2024-01-19 23:24:17 -0500

    Added button functionality and state variables

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from backend import (
 )
 from frontend import stats
 
-
+meal_enter_btn = "ENTER YOUR MEAL"
 
 page = """
 #Hey, Welcome to Dhruv, Liz, Naman and Pooja's Web App
 <br/>
 <|{query}|input|>
-<br/>
+
+<|{meal_enter_btn}|button|on_action=meal_button_press|>
+
 Here are the items you ordered:
 <br/>
 <br/>
@@ -24,6 +26,9 @@ Here are the items you ordered:
 Here is the nutrition data for your meal today:
 <|{nutrition}|>
 
+Here is the total nutrition data for your entire day:
+<|{nutrition_daily}|>
+
 """
 
 pages= {
@@ -32,11 +37,11 @@ pages= {
 }
 stats_page = Html(stats.html_string)
 
-def get_items(query):
+def get_items(api_rep):
     
-    print(f"\n I'm the current query {query} ")
+    print(f"\n I'm the current query")
     
-    list_of_items = get_names_of_meal(get_api_response(query))
+    list_of_items = get_names_of_meal(api_rep)
     return_text = ""
     for index, value in enumerate(list_of_items):
         return_text += value
@@ -46,31 +51,66 @@ def get_items(query):
     print(f"\n I'm the current return text {return_text}")
     return return_text
 
-def get_nutrition(query):
+total_dict_daily = {
+        "Calories": 0.0,
+        "Fat": 0.0,
+        "Protein": 0.0,
+        "Sugar": 0.0,
+        "Fiber": 0.0,
+        "Carbs": 0.0
+    }
+
+def total_nutrition_calc(api_rep):
     
-    print(f"\n I'm the current query AGAIN {query}")
+    global total_dict_daily
     
-    dict_of_n = get_total_info(get_api_response(query))
+    
+    dict_to_add = get_total_info(api_rep)
+
+    for key, value in dict_to_add.items():
+        if key in total_dict_daily:
+            total_dict_daily[key] += value
+            
+    return_text = ""
+    for key, value in total_dict_daily.items():
+        if key == "Calories":
+            return_text += f"{key}: {value} cal \n"
+        else:
+            return_text += f"{key}: {value} grams \n"
+            
+    return return_text
+        
+
+def get_nutrition(api_rep):
+    
+    print(f"\n I'm the current query AGAIN")
+    
+    dict_of_n = get_total_info(api_rep)
     
     return_text = ""
     for key, value in dict_of_n.items():
-        if key == "calories":
+        if key == "Calories":
             return_text += f"{key}: {value} cal \n"
         else:
             return_text += f"{key}: {value} grams \n"
             
     return return_text
 
-def on_change(state, var_name, var_value):
-    if var_name == 'query':
-        print("I'm at on_change ")
-        state.items = get_items(var_value)
-        state.nutrition = get_nutrition(var_value)
+def meal_button_press(state):
+    
+    print(f"\n I'm at meal button press. ")
+    
+    api_rep = get_api_response(state.query)
+    
+    state.items = get_items(api_rep)
+    state.nutrition = get_nutrition(api_rep)
+    state.nutrition_daily = total_nutrition_calc(api_rep)    
     
    
 query = "French Fries"
-items = get_items(query)
-nutrition = get_nutrition(query)     
+items = get_items(get_api_response(query))
+nutrition = get_nutrition(get_api_response(query))
+nutrition_daily = total_nutrition_calc(get_api_response(query))     
 
 
 Gui(pages = pages).run(dark_mode=True, port = 5001)
