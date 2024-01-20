56e5bc4701c27ebc9d7bf86f1069b6b2234e8df3
navronaman <namanmaulikshah@gmail.com>
2024-01-19 23:55:44 -0500

    Initial commit in patch work

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from backend import (
     get_total_info
 )
 from frontend import stats
+from frontend.html_pages import html_page_stats
 
 meal_enter_btn = "ENTER YOUR MEAL"
 
@@ -23,22 +24,19 @@ Here are the items you ordered:
 <br/>
 <br/>
 
-Here is the nutrition data for your meal today:
+### Here is the nutrition data for your meal today:
 <br/>
 <|{nutrition}|>
 
 <br/>
-Here is the total nutrition data for your entire day:
+### Here is the total nutrition data for your entire day:
 <br/>
 <|{nutrition_daily}|>
 
 """
 
-pages= {
-    'home_page': page,
-    'stats_page': Html(stats.html_string)
-}
-stats_page = Html(stats.html_string)
+stats_page = Html(html_page_stats())
+
 
 def get_items(api_rep):
     
@@ -63,6 +61,21 @@ total_dict_daily = {
         "Carbs": 0.0
     }
 
+def get_nutrition(api_rep):
+    
+    print(f"\n I'm the current query AGAIN")
+    
+    dict_of_n = get_total_info(api_rep)
+    
+    return_text = ""
+    for key, value in dict_of_n.items():
+        if key == "Calories":
+            return_text += f"{key}: {value} cal "
+        else:
+            return_text += f"{key}: {value} grams "
+            
+    return return_text
+
 def total_nutrition_calc(api_rep):
     
     global total_dict_daily
@@ -85,21 +98,11 @@ def total_nutrition_calc(api_rep):
             
     return return_text
         
-
-def get_nutrition(api_rep):
+def different_inputs():
     
-    print(f"\n I'm the current query AGAIN")
+    global total_dict_daily
     
-    dict_of_n = get_total_info(api_rep)
-    
-    return_text = ""
-    for key, value in dict_of_n.items():
-        if key == "Calories":
-            return_text += f"{key}: {value} cal "
-        else:
-            return_text += f"{key}: {value} grams "
-            
-    return return_text
+    return total_dict_daily["Calories"], total_dict_daily["Carbs"], total_dict_daily["Sugar"], total_dict_daily["Fat"], total_dict_daily["Fiber"]   
 
 def meal_button_press(state):
     
@@ -109,13 +112,21 @@ def meal_button_press(state):
     
     state.items = get_items(api_rep)
     state.nutrition = get_nutrition(api_rep)
-    state.nutrition_daily = total_nutrition_calc(api_rep)    
+    state.nutrition_daily = total_nutrition_calc(api_rep)
+    
+    stats_page = Html(html_page_stats(different_inputs()))
     
    
 query = ""
 items = get_items(get_api_response(query))
 nutrition = get_nutrition(get_api_response(query))
 nutrition_daily = total_nutrition_calc(get_api_response(query))     
+
+
+pages= {
+    'home_page': page,
+    'stats_page': stats_page
+}
 
 
 Gui(pages = pages).run(dark_mode=True, port = 5001)
