3e01dd773430e9b6e99b9164a0d4b6921001e028
navronaman <namanmaulikshah@gmail.com>
2024-01-20 23:33:45 -0500

    Restructuring functions

--- frontend/html_pages.py
+++ frontend/html_pages.py
@@ -1,11 +1,12 @@
+from taipy.gui import Gui, State
 
-
-def html_page_stats(
-    calorie_counter=0,
-    carbs_counter=0,
-    sugar_counter=0,
-    fat_counter=0,
-    fiber_counter=0):
+def html_page_stats(state, total_dict_daily):
+    
+    calorie_counter = total_dict_daily["Calories"]
+    carbs_counter = total_dict_daily["Carbs"]
+    sugar_counter =  total_dict_daily["Sugar"]
+    fat_counter =  total_dict_daily["Fat"]
+    fiber_counter =  total_dict_daily["Fiber"]
     
     total_calories = 1000
     total_carbs=100
@@ -19,190 +20,196 @@ def html_page_stats(
     fat_percentage = round(fat_counter/total_fat*100)
     fiber_percentage = round(fiber_counter/total_fiber*100)
     
-    html_string = '''
-    <section class = "MainCalorie" id="Calories">
-                <h2>Daily Calorie Intake</h2>
-                <div class = "container">
-                    <div class = "calorie"> '''+ str(calorie_percentage)+ '''%</div>
+    calories_remaining = total_calories-calorie_counter
+    sugar_remaining = total_sugar-sugar_counter
+    carbs_remaining = total_carbs-carbs_counter
+    fat_remaining = total_fat-fat_counter
+    fiber_remaining = total_fiber-fiber_counter
+    
+html_string_stats_0 = '''
+<section class = "MainCalorie" id="Calories">
+            <h2>Daily Calorie Intake</h2>
+            <div class = "container">
+                <div class = "calorie"> <taipy:text> {calorie_percentage} </taipy:text> %</div>
+            </div>
+            <h3> Calorie Limit: {total_calories} </h3>
+            <h4> Calories Remaining: {calories_remaining} </h4>
+        </section>
+        <style>
+            .MainCalorie h2{
+                text-align: center;
+            }
+            .MainCalorie h3{
+                text-align: center;
+            }
+            .MainCalorie h4{
+                text-align: center;
+                color: rgb(256,0,0);
+            }
+            .container{
+                background-color: rgb(192, 192, 192); 
+                width: 80%; 
+                border-radius: 15px; 
+            }
+            .calorie{
+                color: white; 
+                padding: 1%; 
+                text-align: right; 
+                font-size: 20px; 
+                border-radius: 15px; 
+                background-color: rgb(0, 0, 256); 
+                width: {calorie_percentage}%;
+            }
+        </style>
+<title> Other Metrics </title>
+        <style>
+            #gridSection {
+                display: grid;
+                grid-template-columns: repeat(2, 1fr);
+                grid-template-rows: repeat(2, 1fr);
+                gap: 10px;
+                height: 500px; 
+            }
+            .quadrant {
+                padding: 20px;  
+                text-align: center;
+            }
+            .circProgressSugar::before {
+                color: rgb(0,0,256);
+                content: "{sugar_percentage}% \A Total Sugars: {total_sugar}g \A Sugar Remaining: {sugar_remaining}g";
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                width:200px;
+                height: 200px;
+                white-space: pre;
+                font-size: 14px;
+            }
+            .circProgressSugar {
+                position: relative;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-left: 50%;
+                margin-top: 10%;
+                width: 200px;
+                height: 200px;
+                border-radius: 50%;
+                background: 
+                radial-gradient(closest-side, rgb(255, 255, 255) 79%, transparent 80% 100%),
+                conic-gradient(blue {sugar_percentage}%, grey 0);    
+            }
+            .circProgressCarbs::before {
+                color: rgb(0,0,256);
+                content: "{carbs_percentage}% \A Total Carbs: {total_carbs}g \A Sugar Remaining: {carbs_remaining}g";
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                width:200px;
+                height: 200px;
+                white-space: pre;
+                font-size: 14px;
+            }
+            .circProgressCarbs {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-left: 50%;
+                margin-top: 50px;
+                width: 200px;
+                height: 200px;
+                border-radius: 50%;
+                background: 
+                radial-gradient(closest-side, rgb(255, 255, 255) 79%, transparent 80% 100%),
+                conic-gradient(blue {carbs_percentage}%, grey 0);    
+            }
+            .circProgressFiber::before {
+                color: rgb(0,0,256);
+                content: "{fiber_percentage}% \A Total Fibers: {total_fiber}g \A Fiber Remaining: {fiber_remaining}g";
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                width:200px;
+                height: 200px;
+                white-space: pre;
+                font-size: 14px;
+            }
+            .circProgressFiber {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-left: 50%;
+                margin-top: 50px;
+                width: 200px;
+                height: 200px;
+                border-radius: 50%;
+                background: 
+                radial-gradient(closest-side, rgb(255, 255, 255) 79%, transparent 80% 100%),
+                conic-gradient(blue {fiber_percentage}%, grey 0);    
+            }
+            .circProgressFat::before {
+                color: rgb(0,0,256);
+                content: "{fat_percentage}% \A Total Fat: {total_fat}g \A Sugar Remaining: {fat_remaining}g";
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                width:200px;
+                height: 200px;
+                white-space: pre;
+                font-size: 14px;
+            }
+            .circProgressFat {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-left: 50%;
+                margin-top: 50px;
+                width: 200px;
+                height: 200px;
+                border-radius: 50%;
+                background: 
+                radial-gradient(closest-side, rgb(255, 255, 255) 79%, transparent 80% 100%),
+                conic-gradient(blue {fat_percentage}%, grey 0);    
+            }
+            .quadrant h2{
+                margin-left: 20%;
+            }
+        </style>
+        <section id="gridSection">
+            <div class="quadrant">
+            <h2> Sugar </h2>
+                <div class = "circProgressSugar">
+                    <progress value = "30" min ="0" max = "100" style="visibility:hidden;height:0;width:0;"></progress>
                 </div>
-                <h3> Calorie Limit: '''+str(total_calories)+'''</h3>
-                <h4> Calories Remaining: '''+str(total_calories-calorie_counter)+'''</h4>
-            </section>
-            <style>
-                .MainCalorie h2{
-                    text-align: center;
-                }
-                .MainCalorie h3{
-                    text-align: center;
-                }
-                .MainCalorie h4{
-                    text-align: center;
-                    color: rgb(256,0,0);
-                }
-                .container{
-                    background-color: rgb(192, 192, 192); 
-                    width: 80%; 
-                    border-radius: 15px; 
-                }
-                .calorie{
-                    color: white; 
-                    padding: 1%; 
-                    text-align: right; 
-                    font-size: 20px; 
-                    border-radius: 15px; 
-                    background-color: rgb(0, 0, 256); 
-                    width: ''' + str(calorie_percentage)+ '''%;
-                }
-            </style>
-    <title> Other Metrics </title>
-            <style>
-                #gridSection {
-                    display: grid;
-                    grid-template-columns: repeat(2, 1fr);
-                    grid-template-rows: repeat(2, 1fr);
-                    gap: 10px;
-                    height: 500px; 
-                }
-                .quadrant {
-                    padding: 20px;  
-                    text-align: center;
-                }
-                .circProgressSugar::before {
-                    color: rgb(0,0,256);
-                    content: "'''+str(sugar_percentage)+'''% \A Total Sugars: '''+str(total_sugar)+'''g \A Sugar Remaining: '''+str(total_sugar-sugar_counter)+'''g";
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    position: absolute;
-                    width:200px;
-                    height: 200px;
-                    white-space: pre;
-                    font-size: 14px;
-                }
-                .circProgressSugar {
-                    position: relative;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    margin-left: 50%;
-                    margin-top: 10%;
-                    width: 200px;
-                    height: 200px;
-                    border-radius: 50%;
-                    background: 
-                    radial-gradient(closest-side, rgb(255, 255, 255) 79%, transparent 80% 100%),
-                    conic-gradient(blue '''+str(sugar_percentage)+'''%, grey 0);    
-                }
-                .circProgressCarbs::before {
-                    color: rgb(0,0,256);
-                    content: "'''+str(carbs_percentage)+'''% \A Total Carbs: '''+str(total_carbs)+'''g \A Sugar Remaining: '''+str(total_carbs-carbs_counter)+'''g";
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    position: absolute;
-                    width:200px;
-                    height: 200px;
-                    white-space: pre;
-                    font-size: 14px;
-                }
-                .circProgressCarbs {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    margin-left: 50%;
-                    margin-top: 50px;
-                    width: 200px;
-                    height: 200px;
-                    border-radius: 50%;
-                    background: 
-                    radial-gradient(closest-side, rgb(255, 255, 255) 79%, transparent 80% 100%),
-                    conic-gradient(blue '''+str(carbs_percentage)+'''%, grey 0);    
-                }
-                .circProgressFiber::before {
-                    color: rgb(0,0,256);
-                    content: "'''+str(fiber_percentage)+'''% \A Total Fibers: '''+str(total_fiber)+'''g \A Fiber Remaining: '''+str(total_fiber-fiber_counter)+'''g";
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    position: absolute;
-                    width:200px;
-                    height: 200px;
-                    white-space: pre;
-                    font-size: 14px;
-                }
-                .circProgressFiber {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    margin-left: 50%;
-                    margin-top: 50px;
-                    width: 200px;
-                    height: 200px;
-                    border-radius: 50%;
-                    background: 
-                    radial-gradient(closest-side, rgb(255, 255, 255) 79%, transparent 80% 100%),
-                    conic-gradient(blue '''+str(fiber_percentage)+'''%, grey 0);    
-                }
-                .circProgressFat::before {
-                    color: rgb(0,0,256);
-                    content: "'''+str(fat_percentage)+'''% \A Total Fat: '''+str(total_fat)+'''g \A Sugar Remaining: '''+str(total_fat-fat_counter)+'''g";
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    position: absolute;
-                    width:200px;
-                    height: 200px;
-                    white-space: pre;
-                    font-size: 14px;
-                }
-                .circProgressFat {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    margin-left: 50%;
-                    margin-top: 50px;
-                    width: 200px;
-                    height: 200px;
-                    border-radius: 50%;
-                    background: 
-                    radial-gradient(closest-side, rgb(255, 255, 255) 79%, transparent 80% 100%),
-                    conic-gradient(blue '''+str(fat_percentage)+'''%, grey 0);    
-                }
-                .quadrant h2{
-                    margin-left: 20%;
-                }
-            </style>
-            <section id="gridSection">
-                <div class="quadrant">
-                <h2> Sugar </h2>
-                    <div class = "circProgressSugar">
-                        <progress value = "30" min ="0" max = "100" style="visibility:hidden;height:0;width:0;"></progress>
-                    </div>
+            </div>
+            <div class="quadrant">
+            <h2> Carbs </h2>
+            <div class = "circProgressCarbs">
+                    <progress value = "70" min ="0" max = "100" style="visibility:hidden;height:0;width:0;"></progress>
+                    
                 </div>
-                <div class="quadrant">
-                <h2> Carbs </h2>
-                <div class = "circProgressCarbs">
-                        <progress value = "70" min ="0" max = "100" style="visibility:hidden;height:0;width:0;"></progress>
-                        
-                    </div>
+            </div>
+            <div class="quadrant">
+            <h2> Fat </h2>
+            <div class = "circProgressFat">
+                    <progress value = "70" min ="0" max = "100" style="visibility:hidden;height:0;width:0;">30%</progress>
                 </div>
-                <div class="quadrant">
-                <h2> Fat </h2>
-                <div class = "circProgressFat">
-                        <progress value = "70" min ="0" max = "100" style="visibility:hidden;height:0;width:0;">30%</progress>
-                    </div>
-                    </div>
-                <div class="quadrant">
-                <h2> Fiber </h2>
-                <div class = "circProgressFiber">
-                        <progress value = "70" min ="0" max = "100" style="visibility:hidden;height:0;width:0;">30%</progress>
-                    </div>
-                    </div>
-            </section>'''
+                </div>
+            <div class="quadrant">
+            <h2> Fiber </h2>
+            <div class = "circProgressFiber">
+                    <progress value = "70" min ="0" max = "100" style="visibility:hidden;height:0;width:0;">30%</progress>
+                </div>
+                </div>
+        </section>'''
 
 
 
-        
-        
+    
+    
 
 
