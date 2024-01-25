c1395cd3903e3c1d283272751e593afc7fdc1508
navronaman <namanmaulikshah@gmail.com>
2024-01-25 13:06:22 -0500

    Added basic cloud functionality

--- cloud.py
+++ cloud.py
@@ -6,12 +6,34 @@ from firebase_admin import storage
 from datetime import datetime
 
 # Create a folder in the portal called keys, and get your serviceAccoun
-cred = credentials.Certificate("keys/serviceAccountKey.json")
+cred = credentials.Certificate("key/serviceAccountKey.json")
 
 firebase_admin.initialize_app(cred, {
    "databaseURL": "https://calories-tracker-aade3-default-rtdb.firebaseio.com/",
-    "storageBucket": "gs://calories-tracker-aade3.appspot.com"
+    "storageBucket": "calories-tracker-aade3.appspot.com"
 })
 
 
+
+today_date = datetime.now()
+print(today_date)
+
+def inti_cloud():
+    print("Cloud integration started")
+
+    ref = db.reference("Trial")
+    date_ref = ref.child("Hello")
+
+
+    date_ref.set({
+        "User entered a meal at this time": {
+            "meal_items": "french_fries",
+        }
+    })
+
+
+    print(ref.get())
+
+if __name__ == "__main__":
+    inti_cloud()
 
